c851cfdbb48ad1ff0d93d094194a8c9fe6faac79
Brad Fontaine <bradf.83@gmail.com>
2019-10-13 08:47:52 -0600

    Moved the NavBar to its own functional component.

--- src/App.js
+++ src/App.js
@@ -1,42 +1,48 @@
 import React, {useState} from 'react';
 
 function App() {
-    const [hidden, setHidden] = useState(true);
-
-    const smallNavHidden = hidden ? 'hidden' : '';
     return (
         <>
-            <section>
-                <nav className="flex items-center justify-between flex-wrap bg-black p-3">
-                    {/*Brand*/}
-                    <div className="flex items-center flex-shrink-0 text-white mr-6">
-                        <span className="font-semibold text-xl tracking-tight">React Tailwind</span>
-                    </div>
-
-                    {/*Hamburger*/}
-                    <div className="block sm:hidden">
-                        <Hamburger clickHandler={() => setHidden(current => !current)} />
-                    </div>
-
-                    {/*Menu Content*/}
-                    <div className={`w-full block flex-grow sm:flex sm:items-center sm:w-auto ${smallNavHidden}`}>
-                        {/*Left Side Grows*/}
-                        <div className="text-sm sm:flex-grow">
-                            <MenuLink label="Home"/>
-                        </div>
-                        {/*Right Side only takes space it needs*/}
-                        <div>
-                            {/* TODO: Authenticated vs Not */}
-                            <MenuLink label="Sign In"/>
-                        </div>
-                    </div>
-                </nav>
-            </section>
+          <NavBar/>
         </>
     );
 }
 
 export default App;
+
+const NavBar = () => {
+    const [hidden, setHidden] = useState(true);
+
+    const smallNavHidden = hidden ? 'hidden' : '';
+    return (
+        <section>
+            <nav className="flex items-center justify-between flex-wrap bg-black p-3">
+                {/*Brand*/}
+                <div className="flex items-center flex-shrink-0 text-white mr-6">
+                    <span className="font-semibold text-xl tracking-tight">React Tailwind</span>
+                </div>
+
+                {/*Hamburger*/}
+                <div className="block sm:hidden">
+                    <Hamburger clickHandler={() => setHidden(current => !current)} />
+                </div>
+
+                {/*Menu Content*/}
+                <div className={`w-full block flex-grow sm:flex sm:items-center sm:w-auto ${smallNavHidden}`}>
+                    {/*Left Side Grows*/}
+                    <div className="text-sm sm:flex-grow">
+                        <MenuLink label="Home"/>
+                    </div>
+                    {/*Right Side only takes space it needs*/}
+                    <div>
+                        {/* TODO: Authenticated vs Not */}
+                        <MenuLink label="Sign In"/>
+                    </div>
+                </div>
+            </nav>
+        </section>
+    )
+};
 
 const Hamburger = ({clickHandler}) => {
     return (
