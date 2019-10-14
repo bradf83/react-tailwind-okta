3b620e2eea618760705c92eaa2d72f7b555bed0e
Brad Fontaine <bradf.83@gmail.com>
2019-10-13 18:11:31 -0600

    Added purgecss and reconfigured postcss for tailwind.

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,22 @@
 const tailwindcss = require('tailwindcss');
+const purgecss = require('@fullhuman/postcss-purgecss')
+({
+
+    // Specify the paths to all of the template files in your project
+    content: [
+        './src/**/*.html',
+        './src/**/*.js',
+    ],
+    css: ['./src/styles/tailwind.css'],
+    // Include any special characters you're using in this regular expression
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
+
 module.exports = {
     plugins: [
         tailwindcss('./tailwind.config.js'),
         require('autoprefixer'),
+        // purgecss TODO: Put this back as needed
     ],
 };
