b44f935c31dc0cdb3079e86688a104c2d92e9432
Brad Fontaine <bradf.83@gmail.com>
2019-10-14 18:10:00 -0600

    Added a few alert patterns.

--- src/components/patterns/ResponsiveLayouts.js
+++ src/components/patterns/ResponsiveLayouts.js
@@ -6,6 +6,16 @@ import woman from "../../nick-arnot-AuCrXk0pLWo-unsplash.jpg";
 const ResponsiveLayouts = () => {
     return (
         <>
+
+            <div className="bg-blue-100 border-l-4 border-blue-500 text-blue-700 p-4 mb-2 mt-2" role="alert">
+                <p className="font-bold">Patterns!</p>
+                <p className="text-small italic">
+                    A few different patterns I have put together while learning TailwindCSS.  These could easily be extracted
+                    to smaller components on their own but I wanted to make sure I could see how they play with other components
+                    as I was building them.
+                </p>
+            </div>
+
             <div className="sm:flex mb-2">
                 <div
                     className="mb-2 sm:mb-0 sm:mr-2 sm:w-1/4 bg-yellow-200 h-64 p-2 rounded-lg border-2 border-yellow-600 text-yellow-600">
@@ -59,6 +69,23 @@ const ResponsiveLayouts = () => {
                         <p className="mt-2 text-gray-600">Building layouts is a  lot of hard work.
                             This page is a collection of different ideas.</p>
                     </div>
+                </div>
+            </div>
+
+            <div>
+                <div className="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4 mb-2" role="alert">
+                    <p className="font-bold">Warning</p>
+                    <p>Something is not right around here.</p>
+                </div>
+
+                <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-2" role="alert">
+                    <p className="font-bold">Error</p>
+                    <p>You broke the internet, please call support.</p>
+                </div>
+
+                <div className="bg-green-100 border-l-4 border-green-500 text-green-700 p-4 mb-2" role="alert">
+                    <p className="font-bold">Success</p>
+                    <p>You are done!  See you next time.</p>
                 </div>
             </div>
         </>
