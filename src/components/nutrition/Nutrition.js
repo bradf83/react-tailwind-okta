6fda2308015904ed55abe195ecdcaa15311946ef
Brad Fontaine <bradf.83@gmail.com>
2019-11-24 19:34:01 -0700

    Added comments and a possible default approach but it does not seem to work.

--- src/components/nutrition/Nutrition.js
+++ src/components/nutrition/Nutrition.js
@@ -212,8 +212,8 @@ const CustomSelect = ({onChange = () => {}, value = '', children, autoFocus = fa
     )
 };
 
-// This version works but no longer allows us to give defaults
-// const CustomSelect = ({children, ...rest}) => {
+// This version works but no longer allows us to give defaults (method below not working
+// const CustomSelect2 = ({children, ...rest} = {children: undefined, onChange: () => {}, autoFocus: false, title: '', value: ''}) => {
 //     return (
 //         <select {...rest}
 //                 className={"appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"}>
